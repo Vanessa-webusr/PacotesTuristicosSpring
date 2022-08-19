15661b880d5409430890187245ec101a23658039
Vanessa-webusr <alvesnessa42@gmail.com>
2022-08-19 08:49:11 -0300

    repositórios

---
+++ src/main/java/br/ufscar/dc/dsw/PacotesTuristicosSpringApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacotesTuristicosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacotesTuristicosSpringApplication.class, args);
+	}
+
+}
